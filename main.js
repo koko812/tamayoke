9581596861f86ffafee30afd96573d795fbef99e
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-05-03 17:55:02 +0900

    cute_chick_moving!

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const width = 300
 const height = 300
 
-const size = 30
-let heroX = width/2 -size /2
+const heroSize = 30
+let heroX = width/2 -heroSize /2
 let heroY = height/7*5
 let heroElement = null
+
+const updateHero = () => {
+    heroElement.style.top = `${heroY}px`
+    heroElement.style.left = `${heroX}px`
+}
 
 const init = () => {
     const container = document.createElement('div')
@@ -16,15 +21,35 @@ const init = () => {
 
     heroElement = document.createElement('div')
     heroElement.style.position = 'absolute'
-    heroElement.style.top = `${heroY}px`
-    heroElement.style.left = `${heroX}px`
+    updateHero()
     heroElement.style.width = `${width}px`
     heroElement.style.height = `${height}px`
     heroElement.textContent = '🐥'
     heroElement.style.color = '#f00'
-    heroElement.style.fontSize = '30px'
+    //heroElement.style.fontSize = '30px'
+    // size に準じた方が良さげ
+    heroElement.style.fontSize = `${heroSize*0.8}px`
     container.appendChild(heroElement)
     
+    // 基本的に，html 要素に対して，onhogehoge はつけるもんだと覚えておこう
+    // ちゃんと予測変換も出てくる
+    container.ondblclick = (e) =>{
+        e.preventDefault()
+    }
+    // なるほどこれを流用すれば，マウスカーソルのアイコンを変えられるというわけだ
+    // マウスカーソルを消すみたいなやつもきっとどこかにあろう
+    // マウスカーソルにキラキラをつけるみたいなのもきっと実装できるはずだな
+    // デスクトップに可愛い秘書を作るというのもきっとできるはずなのでやってみたい
+    // (pc は重くなってしまいそうなもんだが;)
+
+    // 別にこれでも問題はなさそうなもんだけど，画面外から入ってきたときに瞬間移動するのが気に入らんので
+    // original とかはとっておくことにしよう
+    container.onpointermove = (e) => {
+        heroX = e.pageX
+        heroY = e.pageY
+        console.log('hero');
+        updateHero()
+    }
     
 }
 
