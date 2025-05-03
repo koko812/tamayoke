881adabfa69bf0a408f9ea6033ea729d56598962
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-05-03 18:36:56 +0900

    chick_move_improvement

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const width = 300
 const height = 300
 
 const heroSize = 30
-let heroX = width/2 -heroSize /2
-let heroY = height/7*5
+let heroX = width / 2
+let heroY = height / 2
 let heroElement = null
 
 const updateHero = () => {
@@ -12,6 +12,7 @@ const updateHero = () => {
 }
 
 const init = () => {
+    console.log(heroX, heroY);
     const container = document.createElement('div')
     container.style.position = 'absolute'
     container.style.width = `${width}px`
@@ -21,19 +22,25 @@ const init = () => {
 
     heroElement = document.createElement('div')
     heroElement.style.position = 'absolute'
-    updateHero()
-    heroElement.style.width = `${width}px`
-    heroElement.style.height = `${height}px`
+    heroElement.style.width = `${heroSize}px`
+    heroElement.style.height = `${heroSize}px`
+    heroElement.style.display = 'flex'
+    heroElement.style.justifyContent = 'center'
+    heroElement.style.alignItems = 'center'
     heroElement.textContent = '🐥'
     heroElement.style.color = '#f00'
+    updateHero()
+    console.log(heroX, heroY);
     //heroElement.style.fontSize = '30px'
     // size に準じた方が良さげ
-    heroElement.style.fontSize = `${heroSize*0.8}px`
+    heroElement.style.fontSize = `${heroSize * 0.8}px`
+    // 今はやってないが，センタリングするときに，flex を指定するのはなぜなのだろうか
+    // そもそも flex の効果もよくわかってないところはあるんだけど
     container.appendChild(heroElement)
-    
+
     // 基本的に，html 要素に対して，onhogehoge はつけるもんだと覚えておこう
     // ちゃんと予測変換も出てくる
-    container.ondblclick = (e) =>{
+    container.ondblclick = (e) => {
         e.preventDefault()
     }
     // なるほどこれを流用すれば，マウスカーソルのアイコンを変えられるというわけだ
@@ -42,17 +49,50 @@ const init = () => {
     // デスクトップに可愛い秘書を作るというのもきっとできるはずなのでやってみたい
     // (pc は重くなってしまいそうなもんだが;)
 
+    // まさかの，ここの -1 を指定していなかったら，一周めで heroX に undifined が入って
+    // それ以降の更新でそれに引っ張られて死ぬっぽい，そんな話ある！？クソむずいね
+    let originalX = -1, originalY, originalHeroX, originalHeroY
+    document.onpointerdown = (e) => {
+        e.preventDefault()
+        originalX = e.pageX
+        originalY = e.pageY
+        originalHeroX = heroX
+        originalHeroY = heroY
+        console.log(heroX, heroY);
+    }
     // 別にこれでも問題はなさそうなもんだけど，画面外から入ってきたときに瞬間移動するのが気に入らんので
     // original とかはとっておくことにしよう
-    container.onpointermove = (e) => {
-        heroX = e.pageX
-        heroY = e.pageY
-        console.log('hero');
+    // いつか，コントローラーで操作できるようになどしてみたい気分
+    document.onpointermove = (e) => {
+        e.preventDefault()
+        if (originalX !== -1) {
+            heroX = originalHeroX + (e.pageX - originalX) * 1.5
+            heroY = originalHeroY + (e.pageY - originalY) * 1.5
+            console.log('hero', e.pageX, e.pageY, originalX, originalY, originalHeroX, originalHeroY, heroX, heroY);
+
+            // 画面外に出ない処理，いい感じだ
+            // 他のプログラムでも，こういう門番的なのを綺麗に書いていきたいね
+            // すまん，t-kihira はこれを二行で書いていてカッコ良すぎるので，余力があったら次はパクっておいてほしい
+            // min と max を使ってて本当によくわからん処理を書いていてすごいぞ
+            if (heroX < 0) {
+                heroX = 0
+            }
+            if (heroY < 0) {
+                heroY = 0
+            }
+            if (heroX > width - heroSize) {
+                heroX = width - heroSize
+            }
+            if (heroY > height - heroSize) {
+                heroY = height - heroSize
+            }
+        }
         updateHero()
     }
-    
+    // このままだと，一回クリックしたときに話せないのが面倒
+    // とはいえ，pointer up とかをつけたら，ドラッグ操作になるので，それも面倒
 }
 
-window.onload = () =>{
+window.onload = () => {
     init()
 }
